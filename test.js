97590cdf591ed9199b0b5710bda481a8956e49a1
yanpbt <86374724+yanpbt@users.noreply.github.com>
2021-12-02 17:37:34 +0800

    Update test.js

--- test.js
+++ test.js
@@ -23,9 +23,9 @@ window.addEventListener('click', () => {
     if (num == 10) {
         st()
         ChillamusicDD()
-        setTimeout("alert('祈菈彩蛋 ヽ(°▽°)ノ')", 500)
         setTimeout("sp()", 1000)
-        num=0;
+        num = 0;
+        setTimeout("alert('祈菈彩蛋 ヽ(°▽°)ノ')", 1500)
     }
 })
 const Chilla = new Date(2021, 3, 7);
