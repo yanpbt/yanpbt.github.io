be0b51c527a5f0b56e965568590ff4ff3cec523c
yanpbt <86374724+yanpbt@users.noreply.github.com>
2021-12-07 19:48:12 +0800

    Update test.js

--- test.js
+++ test.js
@@ -29,11 +29,7 @@ function Chillamusicangry() {
 }
 YOASOBI = document.getElementById("YOASOBI");
 YOASOBI.volume = 0.1;
-YOASOBI.play();
-setTimeout(() =>{
-	YOASOBI.pause()
-	YOASOBI.load()
-},10)
+
 //-----------
 //rickroll音樂
 const rickmusic = new Audio("Rick Astley.mp3")
